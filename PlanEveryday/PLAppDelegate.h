b95fb8531ed257d0fd745ac5dbe44e5357794c2c
Hao Zheng <haozheng@Haos-MacBook-Air.local>
2013-04-03 11:09:23 -0500

    Initial Commit

---
+++ PlanEveryday/PLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PLAppDelegate.h
+//  PlanEveryday
+//
+//  Created by Hao Zheng on 4/3/13.
+//  Copyright (c) 2013 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PlanEveryday/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PlanEveryday
+//
+//  Created by Hao Zheng on 4/3/13.
+//  Copyright (c) 2013 Hao Zheng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PLAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PLAppDelegate class]));
+    }
+}
